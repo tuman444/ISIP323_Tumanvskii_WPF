7a7418637cf895f1cd418e17ea1c13733a27a813
tuman <tuman@DIMA>
2026-03-06 11:24:53 +0300

    Подключение бд

---
+++ PR15/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR15
+{
+    public class Core
+    {
+        private static PCdbEntities _db;
+
+        public static PCdbEntities DB
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new PCdbEntities();
+                return _db;
+            }
+        }
+
+    }
+}
