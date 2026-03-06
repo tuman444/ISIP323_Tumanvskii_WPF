66488a3936045e79ddfc1d8dde427e9333fad539
tuman <tuman@DIMA>
2026-03-06 11:34:29 +0300

    Реализация 2 страннцы

--- PR15/Pages/SavedBuildsPage.xaml.cs
+++ PR15/Pages/SavedBuildsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,49 @@ namespace PR15.Pages
         public SavedBuildsPage()
         {
             InitializeComponent();
+            LoadSaveBuilds();
+        }
+        private void LoadSaveBuilds()
+        {
+            using (var db = new PCdbEntities())
+            {
+                SaveBuilds.ItemsSource = db.assembly.ToList();
+            }
+        }
+        private void SaveBuilds_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SaveBuilds.SelectedItem is assembly selected)
+            {
+                TbAuthor.Text = $"Автор сборки: {selected.author}";
+
+                using (var db = new PCdbEntities())
+                {
+                    db.Configuration.LazyLoadingEnabled = false;
+                    db.Configuration.ProxyCreationEnabled = false;
+                    var links = db.partassembly
+                                  .Where(pa => pa.assemblyid == selected.id)
+                                  .Include(pa => pa.basepart)
+                                  .ToList();
+
+                    var finalParts = new List<basepart>();
+                    foreach (var link in links)
+                    {
+                        var part = link.basepart;
+
+
+                        db.Entry(part).Reference(p => p.cpu).Load();
+                        db.Entry(part).Reference(p => p.gpu).Load();
+                        db.Entry(part).Reference(p => p.motherboard).Load();
+                        db.Entry(part).Reference(p => p.ram).Load();
+                        db.Entry(part).Reference(p => p.powersupply).Load();
+                        db.Entry(part).Reference(p => p.storagedevice).Load();
+                        db.Entry(part).Reference(p => p.processorcooler).Load();
+
+                        finalParts.Add(part);
+                    }
+                    SaveBuildsParts.ItemsSource = finalParts;
+                }
+            }
         }
     }
 }
