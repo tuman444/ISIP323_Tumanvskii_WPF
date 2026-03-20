d36ba599f7f4a7f8ed7335b9ea13fe9bec32899b
tuman <tuman@DIMA>
2026-03-20 11:39:46 +0300

    Переделал визуал полностью

--- PR16/MainWindow.xaml.cs
+++ PR16/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace PR16
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+           
         }
     }
 }
